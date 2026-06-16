ab88004e529275aba54c5a3cae28eb3ffa1416df
AntonioCarlos-Dev <antoniocarmo@gmail.com>
2026-06-16 20:27:35 -0300

    Primeiro commit RedFrontier

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import "../styles/Utility.css"
+
+export default function Home() {
+  return (
+   <main className ="container">
+        <h1>Teste</h1>
+        <a href="link aleatorio">LinkTeste</a>
+   </main>
+  )
+}
