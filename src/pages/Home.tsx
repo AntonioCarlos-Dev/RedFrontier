ebf1458ee923293fc8bd92f9fc2d249483a82d8c
Antonio Carlos - TI Coopavel <antonio@coopavel.com.br>
2026-06-22 09:38:34 -0300

    Hero com fundo de marte

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import Close from "../assets/close.svg";
 import Button from "../components/Button";
 import "../styles/header.css";
 import "../styles/utility.css";
+import "../styles/hero.css";
 
 export default function Home() {
     const [showMobileMenu, setShowMobileMenu] = useState(false);
@@ -56,6 +57,22 @@ export default function Home() {
                     </div>
                 </nav>
             </header>
+            <section id="hero">
+    <div className="container content">
+        <p className="desktop-only">RedFrontier</p>
+        <h1>A fronteira vermelha começa aqui.</h1>
+        <p>
+            RedFrontier reúne os dados oficiais das missões marcianas da NASA
+            e os traduz numa jornada interativa de descobertas.
+        </p>
+        <div className="flex gap-1">
+            <span><Button text="Começar a explorar" /></span>
+            <span className="desktop-only">
+                <Button text="Saiba mais" secondary />
+            </span>
+        </div>
+    </div>
+</section>
         </>
     )
 }
